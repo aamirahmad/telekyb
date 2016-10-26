8f4147b5f638c69355de90d4b13ec23cd5ba619c
aamirahmad <aamir.iitkgp@gmail.com>
2016-10-26 17:02:48 +0200

    adding dynamic_reconfigure

--- telekyb_main/telekyb_core/include/telekyb_core/TeleKybCore.hpp
+++ telekyb_main/telekyb_core/include/telekyb_core/TeleKybCore.hpp
@@ -25,6 +25,7 @@
 
 // ros
 #include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
 
 
 namespace TELEKYB_NAMESPACE {
--- telekyb_main/telekyb_core/src/telekyb_core/Main.cpp
+++ telekyb_main/telekyb_core/src/telekyb_core/Main.cpp
@@ -6,11 +6,20 @@
  */
 
 #include <telekyb_core/TeleKybCore.hpp>
+#include <telekyb_core/UAVcontrolParametersConfig.h>
 
 #include <telekyb_base/TeleKyb.hpp>
 #include <ros/ros.h>
 
 // main
+
+float positionControl_tDerivGain;
+
+void reconf_callback(TeleKybCore::UAVcontrolParametersConfig &config, uint32_t level)
+{
+    positionControl_tDerivGain = config.positionControl_tDerivGain;
+}
+
 int main(int argc, char **argv) {
 
 // This has to run with a few more threads
@@ -18,6 +27,11 @@ int main(int argc, char **argv) {
 	telekyb::TeleKyb::init(argc,argv,"TeleKybCore", ros::init_options::AnonymousName);
 
 	telekyb::TeleKybCore* core = new telekyb::TeleKybCore();
+	
+	dynamic_reconfigure::Server<TeleKybCore::UAVcontrolParametersConfig> server;
+	dynamic_reconfigure::Server<TeleKybCore::UAVcontrolParametersConfig>::CallbackType f;
+	f = boost::bind(&reconf_callback, _1, _2);
+	server.setCallback(f);	
 
 	// wait for shutdown.
 	ros::waitForShutdown();
